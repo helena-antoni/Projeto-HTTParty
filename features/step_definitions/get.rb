9d7d9a2703e4cda2d4de7aa0ad2e50904a28db35
helena-antoni <helenaantoni03@gmail.com>
2025-09-13 19:24:39 -0300

    feat: Adds GET method for employee lookup

--- features/step_definitions/get.rb
+++ features/step_definitions/get.rb
@@ -1,13 +1,13 @@
 Dado('que o usuario consulte informacoes de funcionarios') do
-    @get_url= ('https://jsonplaceholder.typicode.com/posts/1') 
-
+    @get_url = 'https://jsonplaceholder.typicode.com/posts'
+    puts @teste
 end
 
 Quando('ele realizar a pesquisa') do
     @list_employee = HTTParty.get(@get_url)
 end
 
-Entao('uma lista de funcionarios deve retornar') do
-    expect(@list_employee.code).to eq 200
-    expect(@list_employee.message).to eq 'OK'
+Entao('uma lista de funcionarios deve retornar') do 
+    expect(@list_employee.code).to eql 200
+    expect(@list_employee.message).to eql 'OK'
 end
