6c5472e5a3359a01794aa0d98c249eaa55995837
helena-antoni <helenaantoni03@gmail.com>
2025-09-16 09:00:06 -0300

    Fix: Fixed syntax error in base_url.rb

--- features/support/base_url.rb
+++ features/support/base_url.rb
@@ -2,5 +2,5 @@ module Employee
     include HTTParty
     base_uri 'https://jsonplaceholder.typicode.com/'
     format :json
-    headers 'Content-Type' => 'application/json', 'Authorization'
+    headers 'Content-Type' => 'application/json' 
 end
